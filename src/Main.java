76a4dd5c21187a6cb2e2179fe9002183a403a240
Christian Gracia <christiangracia.student@careerdevs.com>
2019-11-06 00:43:06 -0500

    Set up chrome webdriver with selenium, program can log into yahoo finance already

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\chris\\Downloads\\chromedriver_win32-78\\chromedriver.exe");
+		Scraper scraper = new Scraper();
+		scraper.goToSignIn();
+		scraper.loginUser("cgscraper@yahoo.com", "chris42908!");
+		scraper.quitDriver();
+	}
+}
--- src/Scraper.java
+++ src/Scraper.java
@@ -1,9 +1,31 @@
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Scraper {
 
-	public static void main(String[] args) {
-		//todo
+    public WebDriver Driver = new ChromeDriver();
 
-	}
-
+    public void goToSignIn() {
+        Driver.get("https://login.yahoo.com/config/login?.intl=us&.lang=en-US&.src=finance&.done=https%3A%2F%2Ffinance.yahoo.com%2Fportfolios");
+    }
+    public void loginUser(String user, String password) {
+        WebElement userNameField = Driver.findElement(By.id("login-username"));
+        userNameField.sendKeys(user);
+        userNameField.submit();
+        
+        Driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        
+        WebElement passwordField = Driver.findElement(By.cssSelector("input[id='login-passwd']"));
+        passwordField.sendKeys(password);
+        passwordField.submit();
+    }
+    public void quitDriver() {
+        Driver.quit();
+    }
 }
